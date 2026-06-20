e34da008271f26b36e69ee9eb9207843a3ee7acd
melek saidani <mbcmelek3@gmail.com>
2026-06-20 04:35:27 -0700

    Update omni_B85-D.mk.mk

--- omni_B85-D.mk.mk
+++ omni_B85-D.mk.mk
@@ -5,9 +5,6 @@ $(call inherit-product, build/target/product/embedded.mk)
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
-PRODUCT_COPY_FILES += \
-    device/Haier/B85/recovery.fstab:recovery/root/etc/recovery.fstab
-
 PRODUCT_DEVICE := Haier
 PRODUCT_NAME := omni_B85
 PRODUCT_BRAND := B85
