70f1edef123ef6f3f591bbe84b3b5920215d75e9
melek saidani <mbcmelek3@gmail.com>
2026-06-20 04:58:21 -0700

    Update PRODUCT_MODEL  to B85

--- omni_B85-D.mk
+++ omni_B85-D.mk
@@ -1,3 +1,18 @@
+#
+# Copyright 2026 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 # Embedded
 $(call inherit-product, build/target/product/embedded.mk)
@@ -8,5 +23,5 @@ $(call inherit-product, vendor/omni/config/common.mk)
 PRODUCT_DEVICE := Haier
 PRODUCT_NAME := omni_B85
 PRODUCT_BRAND := B85
-PRODUCT_MODEL := X5
+PRODUCT_MODEL := B85
 PRODUCT_MANUFACTURER := Haier
